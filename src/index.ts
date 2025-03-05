f92c31ed4794bbf2fa516178b7007ac77c6e5708
StarsSwift <AuraElicase@outlook.com>
2025-03-05 21:00:01 +0800

    upload project struct

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,6 @@ export interface Config {}
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.command('echo <message>')
+  .action((_, message) => message)
 }
